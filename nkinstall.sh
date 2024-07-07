d97b3e0dbef42b4ce3ad4c414badaa240c9847dc
TXIAOSHEN <118404203+TXIAOSHEN@users.noreply.github.com>
2024-07-07 19:26:47 +0800

    Update nkinstall.sh

--- nkinstall.sh
+++ nkinstall.sh
@@ -1,12 +1,9 @@
 apt update -y && apt upgrade
-apt install sudo language-pack-zh-hans language-pack-zh-hans-base openjdk-17-jre wget locale
-mkdir /nkpm
-cd /nkpm
-wget https://ghproxy.net/github.com/PetteriM1/NukkitPetteriM1Edition/releases/download/3419/original-nukkit-1.0-SNAPSHOT.jar
-wget https://ghproxy.net/github.com/PetteriM1/NukkitPetteriM1Edition/releases/download/3419/Launcher.jar
-echo "java -jar Launcher.jar" /nkpm/nkstart.sh
-echo "Select zh_ CN UTF-8"
-dpkg reconfigure locale
+apt install language-pack-zh-hans language-pack-zh-hans-base openjdk-17-jre wget locale
+mkdir /nk
+cd /nk
+wget https://ci.opencollab.dev//job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
+echo "java -jar nukkit-1.0-SNAPSHOT.jar" /nkpm/nkstart.sh
 echo "安装完成"
-echo "工作目录为/nkpm，启动命令为:cd /nkpm sh nkstart.sh"
+echo "工作目录为/nkpm，启动命令为:cd /nkpm & sh nkstart.sh"
 echo "开源地址:http://github.com/txiaoshen/nukkit-easyinstall-script"
