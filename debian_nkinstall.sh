96c233b5e19593f594a7f23ee0ae614cc54f5e07
TXIAOSHEN <118404203+TXIAOSHEN@users.noreply.github.com>
2024-07-11 09:31:47 +0800

    Update debian_nkinstall.sh

--- debian_nkinstall.sh
+++ debian_nkinstall.sh
@@ -3,6 +3,7 @@ apt install openjdk-17-jre wget -y
 mkdir /nk
 cd /nk
 wget https://ci.opencollab.dev//job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
+touch nkstart.sh
 echo "java -jar nukkit-1.0-SNAPSHOT.jar" /nk/nkstart.sh
 chmod 777 /nk/nkstart.sh
 echo "安装完成"
