3a7f517038293f6aaab6ee3c243a8ef993fbc443
TXIAOSHEN <118404203+TXIAOSHEN@users.noreply.github.com>
2024-07-21 09:39:42 +0800

    Update arch_nkinstall.sh

--- arch_nkinstall.sh
+++ arch_nkinstall.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 pacman -Syu
-pacman -S openjdk-17-jre wget
+pacman -S jdk17-openjdk wget
 mkdir /nk
 cd /nk
 wget https://ci.opencollab.dev//job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
 touch nkstart.sh
-echo "java -jar nukkit-1.0-SNAPSHOT.jar" /nk/nkstart.sh
+echo "java -jar nukkit-1.0-SNAPSHOT.jar" > nkstart.sh
 chmod 777 /nk/nkstart.sh
 echo "安装完成"
 echo "工作目录为/nk，启动命令为:cd /nk & sh nkstart.sh"
