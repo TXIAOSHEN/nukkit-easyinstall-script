cebc85ab0031df144080d885d9cce755eb2465a7
TXIAOSHEN <118404203+TXIAOSHEN@users.noreply.github.com>
2024-08-24 08:35:16 +0800

    Update arch_nkinstall.sh

--- arch_nkinstall.sh
+++ arch_nkinstall.sh
@@ -4,9 +4,8 @@ pacman -S jdk17-openjdk wget
 mkdir /nk
 cd /nk
 wget https://ci.opencollab.dev//job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
-touch nkstart.sh
-echo "java -jar nukkit-1.0-SNAPSHOT.jar" > nkstart.sh
-chmod 777 /nk/nkstart.sh
+wget https://raw.githubusercontent.com/TXIAOSHEN/nukkit-easyinstall-script/main/nkstart.sh
+chmod 777 nkstart.sh
 echo "安装完成"
 echo "工作目录为/nk，启动命令为:cd /nk & sh nkstart.sh"
 echo "开源地址:http://github.com/txiaoshen/nukkit-easyinstall-script"
