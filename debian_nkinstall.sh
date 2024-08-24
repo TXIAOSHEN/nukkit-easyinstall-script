90e30cce22b6f458adbee52edad5a31fcce95c3d
TXIAOSHEN <118404203+TXIAOSHEN@users.noreply.github.com>
2024-08-24 08:27:06 +0800

    Update debian_nkinstall.sh

--- debian_nkinstall.sh
+++ debian_nkinstall.sh
@@ -4,9 +4,9 @@ apt install openjdk-17-jre wget -y
 mkdir /nk
 cd /nk
 wget https://ci.opencollab.dev//job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
-touch nkstart.sh
-echo "java -jar nukkit-1.0-SNAPSHOT.jar" > nkstart.sh
-chmod 777 /nk/nkstart.sh
+
+chmod 777 nkstart.sh
+cd
 echo "安装完成"
 echo "工作目录为/nk，启动命令为:cd /nk & sh nkstart.sh"
 echo "开源地址:http://github.com/txiaoshen/nukkit-easyinstall-script"
